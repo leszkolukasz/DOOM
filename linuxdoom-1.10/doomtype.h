c7e7050f18f2622b08f2a1633878f5fce64efc73
Łukasz Leszko <leszko.lucas@gmail.com>
2025-08-22 22:43:02 +0200

    Initial fixes

--- linuxdoom-1.10/doomtype.h
+++ linuxdoom-1.10/doomtype.h
@@ -31,7 +31,9 @@
 #ifdef __cplusplus
 typedef bool boolean;
 #else
-typedef enum {false, true} boolean;
+// typedef enum {false, true} boolean;
+#include <stdbool.h>
+typedef bool boolean;
 #endif
 typedef unsigned char byte;
 #endif
